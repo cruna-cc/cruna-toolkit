1a87259d8d746516a555b23bf6ac44f042c422ff
Francesco Sullo <sullof@gmail.com>
2023-05-04 20:10:24 -0700

    Moving to toolkit

--- contracts/mocks/MyOwnedContract.sol
+++ contracts/mocks/MyOwnedContract.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "../NFTOwned.sol";
+import "../nft-owned/NFTOwned.sol";
 
 contract MyOwnedContract is NFTOwned {
 
